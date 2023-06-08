8affdce761318ebf62416e88d12023b90d34f173
DostonDev <85177665+DostonDev@users.noreply.github.com>
2023-06-08 20:07:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const fileupload = require('express-fileupload')
 const cookieparser = require('cookie-parser')
@@ -8,6 +9,7 @@ const cookieparser = require('cookie-parser')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(cors())
 app.use(cookieparser())
 app.use(fileupload({
     useTempFiles: true
